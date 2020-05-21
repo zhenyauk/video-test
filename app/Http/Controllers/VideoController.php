7a11b636819bdbdec852c1ff46a10389a4948cf0
Eugene <zhenyauk@gmail.com>
2020-05-21 11:07:06 +0300

    init

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\Videos\ConvertForStreaming;
+use App\Video;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use FFMpeg;
+
+class VideoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $video = new Video();
+        $video->title = Str::random(4);
+        $video->file =$request->video->store('my');
+        $video->save();
+
+
+      FFMpeg::fromDisk('local')->open($video->file)->save('sdsd.m3u8');
+
+
+        return "ok";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Video  $video
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Video $video)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Video  $video
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Video $video)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Video  $video
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Video $video)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Video  $video
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Video $video)
+    {
+        //
+    }
+}
---
+++ app/Jobs/Videos/ConvertForStreaming.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs\Videos;
+
+use App\Video;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use FFMpeg\Format\Video\X264;
+
+class ConvertForStreaming implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $video;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Video $video)
+    {
+        $this->video = $video;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $low = ( new X264('aac') )->setKiloBitrate(100); //360p
+        //$med = ( new X264('aac') )->setKiloBitrate(250);
+        //$high = ( new X264('aac') )->setKiloBitrate(500);
+
+
+
+
+    }
+}
